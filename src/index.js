c6de1375256032c8c9d40ce2360017ead520fc48
bobcortez <bob.men.cor@gmail.com>
2019-06-05 13:53:46 -0600

    More Accurate titling

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import * as tracker from './assignmentTracker';
 import * as code from './codeFormatter';
 
-let html = document.getElementById("app");
+let body = document.getElementById("app");
 //Figure out NavLink stuff
 let navLinks = [];
 
-html.appendChild(tracker.buildTrackerHtml());
-html.appendChild(code.buildFormatterHtml());
+
+
+body.appendChild(tracker.buildTrackerHtml());
+body.appendChild(code.buildFormatterHtml());
